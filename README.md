b1f75bef6bc7cf7e0b9dad018e69fe4f029c0f3a
zkzong <zongzhankui@hotmail.com>
2019-05-29 23:46:44 +0800

    打包成jar

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # todo-gradle
+
+## 打包成jar
+Tasks -> build -> jar
+
+打包后的文件放在build/libs目录下
+
+使用下面命令执行：
+```
+java -classpath build/libs/todo-gradle-1.0-SNAPSHOT.jar com.zkzong.gradle.todo.App
+```
---
+++ src/main/java/com/zkzong/gradle/todo/TodoItem.java
@@ -0,0 +1,34 @@
+package com.zkzong.gradle.todo;
+
+public class TodoItem {
+
+    // 待办事项名称
+    private String name;
+    // 已完成
+    private boolean hasDone;
+
+    public TodoItem(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isHasDone() {
+        return hasDone;
+    }
+
+    public void setHasDone(boolean hasDone) {
+        this.hasDone = hasDone;
+    }
+
+    @Override
+    public String toString() {
+        return name + (hasDone ? " hasDone" : " need to do") + "!";
+    }
+}
